ebb1ca23ac0da1e84e55968ed426540d0f871124
alphalemon <user@server.com>
2013-04-16 20:31:29 +0200

    updated to uset the AssetsFolder class

--- Controller/MediaLibraryController.php
+++ Controller/MediaLibraryController.php
@@ -3,6 +3,7 @@
 namespace AlphaLemon\Block\CKEditorBlockBundle\Controller;
 
 use AlphaLemon\AlphaLemonCmsBundle\Controller\AlCmsController;
+use AlphaLemon\AlphaLemonCmsBundle\Core\AssetsPath\AlAssetsPath;
 
 class MediaLibraryController extends AlCmsController
 {
@@ -12,7 +13,7 @@ class MediaLibraryController extends AlCmsController
         
         return $this->container->get('templating')->renderResponse('CKEditorBlockBundle:Elfinder:media_library.html.twig', array(
             'enable_yui_compressor' => $this->container->getParameter('alpha_lemon_cms.enable_yui_compressor'),
-            'assets_folder' => $this->container->getParameter('alpha_lemon_cms.upload_assets_dir'),
+            'assets_folder' => AlAssetsPath::getUploadFolder($this->container),
             'frontController' => $this->getFrontcontroller($request),
         ));
     }
